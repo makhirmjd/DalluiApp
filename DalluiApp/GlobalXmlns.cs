27aafbc934620639cd19109eb0e1f0f0c881c98c
Muhammad Abdulmalik <makhirmjd@gmail.com>
2025-09-05 15:35:29 +0100

    Add project files.

---
+++ DalluiApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DalluiApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DalluiApp.Pages")]
